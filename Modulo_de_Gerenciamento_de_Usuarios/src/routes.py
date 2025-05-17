479afc650b4c17509b67870fbb6d73a17eb0c0a9
Thiago Marcos da Silva <thiagobossun06@gmail.com>
2025-05-17 17:25:58 -0300

    Começo do formulario de cadastro + ajuste nas responses

--- Modulo_de_Gerenciamento_de_Usuarios/src/routes.py
+++ Modulo_de_Gerenciamento_de_Usuarios/src/routes.py
@@ -16,12 +16,17 @@ def create_user():
         user = UserService.create(request.get_json())
         return make_response(jsonify(user.json()), 201)
     except Exception as e:
-        return make_response(jsonify({'error': str(e)}), 400)
+        print(f"Error creating user: {e}")
+        return make_response(jsonify({'Usuario já existe!'}), 400)
 
 @bp.route('/users/all', methods=['GET'])
 def get_users():
     try:
         users = UserService.get_all()
+        
+        if not users:
+            return make_response(jsonify({'message': 'no users found'}), 404)
+        
         return make_response(jsonify([u.json() for u in users]), 200)
     except Exception as e:
         return make_response(jsonify({'error': str(e)}), 400)
